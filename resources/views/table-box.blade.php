6d204c68a4e14a3f653f473574c710898c7504db
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-02-22 17:34:00 +0200

    Handle when table empty in table-box component

--- resources/views/table-box.blade.php
+++ resources/views/table-box.blade.php
@@ -16,6 +16,14 @@
 
     <table class="table">
         {{ $slot }}
+        @unless ($model->count())
+            <tr>
+                <td colspan="100%" style="text-align: center;">
+                    No data available.
+                </td>
+            </tr>
+        @endunless
+        @endif
     </table>
 
     <!-- /.box-body -->
