0857471c012bb798feac7996916904a2c095c1a2
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-02-22 14:40:18 +0200

    Update footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,3 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        AdminLTE <b>Version</b> 2.4.0
-    </div>
-    Laravel AdminLTE.
+    {{ config('app.name', 'Laravel AdminLTE.') }}
 </footer>
