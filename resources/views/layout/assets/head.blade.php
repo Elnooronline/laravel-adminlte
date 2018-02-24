5d2656dd0fd31f1d334c843ad761c73237950d84
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-02-24 15:25:55 +0200

    Fix asset urls issue

--- resources/views/layout/assets/head.blade.php
+++ resources/views/layout/assets/head.blade.php
@@ -4,13 +4,13 @@
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href="/vendor/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="{{ url('vendor/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="/vendor/adminlte/bower_components/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ url('vendor/adminlte/bower_components/font-awesome/css/font-awesome.min.css') }}">
 <!-- Ionicons -->
-<link rel="stylesheet" href="/vendor/adminlte/bower_components/Ionicons/css/ionicons.min.css">
+<link rel="stylesheet" href="{{ url('vendor/adminlte/bower_components/Ionicons/css/ionicons.min.css') }}">
 <!-- Theme style -->
-<link rel="stylesheet" href="/vendor/adminlte/dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="{{ url('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
 
 @stack('assets.head')
 
