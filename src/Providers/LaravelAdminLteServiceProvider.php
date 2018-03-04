74669c289e8ba605246083a1f57965774203e6ab
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-03-04 11:02:53 +0200

    Update Service provider to publish assets from vendor directories

--- src/Providers/LaravelAdminLteServiceProvider.php
+++ src/Providers/LaravelAdminLteServiceProvider.php
@@ -28,12 +28,27 @@ class LaravelAdminLteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishConfig();
+
         $this->publishBreadcrumbs();
+
+        $this->publishViews();
+
+        $this->publishAuthScaffolding();
+
         $this->loadViews();
+
         $this->loadTranslations();
+
+        $this->publishTranslations();
+
         $this->publishAssets();
     }
 
+    /**
+     * Publish the configuration files.
+     *
+     * @return void
+     */
     private function publishConfig()
     {
         $adminltePath = $this->packagePath('config/adminlte.php');
@@ -42,50 +57,91 @@ class LaravelAdminLteServiceProvider extends ServiceProvider
         $this->publishes([
             $adminltePath => config_path('adminlte.php'),
             $breadcrumbsPath => config_path('breadcrumbs.php'),
-        ], 'adminlte.config');
+        ], 'adminlte-config');
 
         $this->mergeConfigFrom($adminltePath, 'adminlte');
         $this->mergeConfigFrom($breadcrumbsPath, 'breadcrumbs');
     }
 
+    /**
+     * Publish breadcrumbs definition files.
+     *
+     * @return void
+     */
     private function publishBreadcrumbs()
     {
         $this->publishes([
             $this->packagePath('routes/breadcrumbs.php') => base_path('routes/breadcrumbs.php'),
-        ], 'adminlte.breadcrumbs');
+        ], 'adminlte-breadcrumbs');
     }
 
+    /**
+     * Define the views aliases.
+     */
     private function loadViews()
     {
-        $this->loadViewsFrom($path = $this->packagePath('resources/views'), 'adminlte');
+        $this->loadViewsFrom($this->packagePath('resources/views'), 'adminlte');
+    }
 
+    /**
+     * Publish the views.
+     *
+     * @return void
+     */
+    private function publishViews()
+    {
         $this->publishes([
-            $path => base_path('resources/views/vendor/adminlte'),
-        ], 'adminlte.views');
+            $this->packagePath('resources/views') => base_path('resources/views/vendor/adminlte'),
+        ], 'adminlte-views');
+    }
 
+    /**
+     * Publish the auth scaffolding.
+     *
+     * @return void
+     */
+    private function publishAuthScaffolding()
+    {
         $this->publishes([
             $this->packagePath('resources/stubs/auth/login.blade.php.stub') => base_path('resources/views/auth/login.blade.php'),
             $this->packagePath('resources/stubs/auth/register.blade.php.stub') => base_path('resources/views/auth/register.blade.php'),
             $this->packagePath('resources/stubs/auth/passwords/email.blade.php.stub') => base_path('resources/views/auth/passwords/email.blade.php'),
             $this->packagePath('resources/stubs/auth/passwords/reset.blade.php.stub') => base_path('resources/views/auth/passwords/reset.blade.php'),
             $this->packagePath('resources/stubs/home.blade.php.stub') => base_path('resources/views/home.blade.php'),
-        ], 'adminlte.auth');
+        ], 'adminlte-auth');
     }
 
+    /**
+     * Load the translation files.
+     *
+     * @return void
+     */
     private function loadTranslations()
     {
-        $this->loadTranslationsFrom($path = $this->packagePath('resources/lang'), 'adminlte');
-        $this->publishes([
-            $path => base_path('resources/lang/vendor/adminlte'),
-        ], 'adminlte.translations');
+        $this->loadTranslationsFrom($this->packagePath('resources/lang'), 'adminlte');
     }
 
+    /**
+     * Publish translation files.
+     *
+     * @return void
+     */
+    private function publishTranslations()
+    {
+        $this->publishes([
+            $this->packagePath('resources/lang') => base_path('resources/lang/vendor/adminlte'),
+        ], 'adminlte-translations');
+    }
 
+    /**
+     * Publish adminlte assets.
+     */
     private function publishAssets()
     {
         $this->publishes([
             $this->packagePath('resources/assets') => public_path('vendor/adminlte'),
-        ], 'adminlte.assets');
+            base_path('vendor/vendor/adminlte/almasaeed2010') => public_path('vendor/adminlte'),
+        ], 'adminlte-assets');
     }
 
     private function packagePath($path)
