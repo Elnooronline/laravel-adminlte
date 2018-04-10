1281ef525253b2afc23a21e98e10538ce1b58937
Ahmed Al-Shahawi <alshahawi@outlook.com>
2018-04-10 15:52:28 +0200

    Fix bs-tabs-validator.js

--- resources/assets/js/helpers/bs-tabs-validator.js
+++ resources/assets/js/helpers/bs-tabs-validator.js
@@ -1,4 +1,4 @@
-$('input').on('invalid', () => {
-  let tabId = $('input:invalid').eq(0).closest('[role=tabpanel]').attr('id');
+$('input, textarea').on('invalid', () => {
+  let tabId = $('input:invalid, textarea:invalid').eq(0).closest('[role=tabpanel]').attr('id');
   $(`a[aria-controls="${tabId}"]`).click();
 });
